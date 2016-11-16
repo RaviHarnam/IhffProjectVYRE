e0b0b338e5693f4fc8b58cbe04f807428ccd6899
Vincent <Vincent>
2016-11-16 15:36:40 +0100

    IhffContext toegevoegd

--- IHFF project VYRE/IHFF/Models/IhffContext.cs
+++ IHFF project VYRE/IHFF/Models/IhffContext.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace IHFF.Models
 {
-    public class IhffContext
+    public class IhffContext : DbContext
     {
-        public IhffContext() // : base("Connectiestring")
+        public IhffContext()  : base("IhffConnection")
         {
             // LEEG
         }
