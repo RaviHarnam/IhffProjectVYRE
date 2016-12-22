50a7e9b9d7baa746d75cfc01427af6012ece1994
YBStolker <1134ybs@gmail.com>
2016-12-22 11:43:43 +0100

    categorie op een aantal plekken weggehaald

--- IHFF/Models/Movie.cs
+++ IHFF/Models/Movie.cs
@@ -29,7 +29,7 @@ namespace IHFF.Models
         [NotMapped]
         public virtual Event movieEvent { get; set; }
 
-        public Movie(string rating, string director, string stars, string writers, string categorie, string titel, string omschrijving, bool highlight) : base(titel, omschrijving, highlight)
+        public Movie(string rating, string director, string stars, string writers, string titel, string omschrijving, bool highlight) : base(titel, omschrijving, highlight)
         {
             Rating = rating;
             Director = director;
