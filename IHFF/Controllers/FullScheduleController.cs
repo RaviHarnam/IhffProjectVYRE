f9503bb8f01f38b2ea9adaeb4a7fcac4489b6721
RaviHarnam <harnam@upcmail.nl>
2016-12-28 12:21:04 +0100

    bla

---
+++ IHFF/Controllers/FullScheduleController.cs
@@ -0,0 +1,32 @@
+﻿using IHFF.Enum;
+using IHFF.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IHFF.Controllers
+{
+    public class FullScheduleController : Controller
+    {
+        DbScheduleRepository repository = new DbScheduleRepository();
+        // GET: FullSchedule
+        public ActionResult Index()
+        {
+            return RedirectToAction("FullSchedule");
+        }
+
+        public ActionResult FullSchedule(Weekday? day)
+        {
+            if(day == null)
+            {
+                day = Enum.Weekday.Maandag;
+                ;
+            }           
+            repository.getAllVoorstellingen(day)
+           
+            return View();
+        }
+    }
+}
---
+++ IHFF/Repositories/DbScheduleRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IHFF.Models;
+
+namespace IHFF.Repositories
+{
+    public class DbScheduleRepository : IScheduleRepository
+    {
+        private IhffContext ctx = new IhffContext();
+        public IEnumerable<Voorstelling> getAllVoorstellingen()
+        {
+            IEnumerable<Voorstelling> voorstellingen = ctx.VOORSTELLINGEN.ToList();
+            foreach (Voorstelling voorstelling in voorstellingen)
+            {
+                //switch (voorstelling.DatumTijd.DayOfWeek)
+                //{
+                //    case DayOfWeek.Monday:
+                //        voorstelling.day = Enum.Weekday.Maandag;
+                //        break;
+                //    case DayOfWeek.Tuesday:
+                //        voorstelling.day = Enum.Weekday.Dinsdag;
+                //        break;
+                //    case DayOfWeek.Wednesday:
+                //        voorstelling.day = Enum.Weekday.Woensdag;
+                //        break;
+                //    case DayOfWeek.Thursday:
+                //        voorstelling.day = Enum.Weekday.Donderdag;
+                //        break;
+                //    case DayOfWeek.Friday:
+                //        voorstelling.day = Enum.Weekday.Vrijdag;
+                //        break;
+                //    case DayOfWeek.Saturday:
+                //        voorstelling.day = Enum.Weekday.Zaterdag;
+                //        break;
+                //    case DayOfWeek.Sunday:
+                //        voorstelling.day = Enum.Weekday.Zondag;
+                //        break;
+                //}
+            }
+            return voorstellingen;
+        }
+
+
+    }
+}
