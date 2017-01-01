05c32d5ac79cf4b62439425a1ebf7f5aedb8bafc
yannickstolker <1134ybs@gmail.com>
2017-01-01 16:29:59 +0100

    begin gemaakt aan museum pagina (repo, ctrl, html, css)

---
+++ IHFF/Repositories/IMuseumRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IHFF.Models;
+
+namespace IHFF.Repositories
+{
+    interface IMuseumRepository
+    {
+        List<Museum> GetAll();
+        void AddMuseum(Museum museum);
+        void DeleteMuseum(Museum museum);
+    }
+}
