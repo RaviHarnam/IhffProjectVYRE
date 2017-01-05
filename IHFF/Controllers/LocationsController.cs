d92f59a1761a7ed011b04ddf66c80d244ded70a6
Eise-PC\Eise <eise.wielinga@gmail.com>
2017-01-05 21:59:36 +0100

    Locations rrrr en klein beetje locations

---
+++ IHFF/Controllers/LocationsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IHFF.Controllers
+{
+    public class LocationsController : Controller
+    {
+        // GET: Locations
+        public ActionResult Index()
+        {
+            return RedirectToAction("Locations");
+        }
+
+        public ActionResult Locations()
+        {
+            return View();
+        }
+    }
+}
