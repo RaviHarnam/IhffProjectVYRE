0edbb24130fd87833f3e4458289847a20e2e9304
Eise-PC\Eise <eise.wielinga@gmail.com>
2017-01-09 20:56:23 +0100

    combo

---
+++ IHFF/Models/ComboDeal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IHFF.Models
+{
+    public class ComboDeal
+    {
+        public Movie film1 { get; set; }
+        public Voorstelling voorstelling1 { get; set; }
+        public Movie film2 { get; set; }
+        public Voorstelling voorstelling2 { get; set; }
+        public Special special { get; set; }
+        public Voorstelling voorstelling3 { get; set; }
+
+        public ComboDeal() { }
+    }
+}
---
+++ IHFF/Repositories/DbItemRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IHFF.Models;
+
+namespace IHFF.Repositories
+{
+    public class DbItemRepository : IItemRepository
+    {
+        private IhffContext ctx = new IhffContext();
+        public IEnumerable<Item> GetAllItems()
+        {
+            IEnumerable<Item> items = ctx.ITEMS.ToList();
+            foreach (Item item in items)
+                item.ItemVoorstelling = (from v in ctx.VOORSTELLINGEN
+                                         where v.ItemId == item.ItemID
+                                         select v).ToList();
+            return items;
+        }
+    }
+}
